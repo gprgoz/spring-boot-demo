2d3bb6f5d642bd81150fb130cbc582b0fceee30b
keketrtr <keketrtr@126.com>
2018-06-30 07:42:21 +0800

    端口

--- src/main/java/com/example/demo/web/Example.java
+++ src/main/java/com/example/demo/web/Example.java
@@ -1,5 +1,7 @@
 package com.example.demo.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,9 +10,13 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class Example {
+    @Autowired
+    private ApplicationArguments applicationArguments;
+
     @RequestMapping("/")
     public String home(){
         System.out.println("=====================6666666666666666");
+        System.out.println(applicationArguments.getNonOptionArgs());
         return "Hello World!";
     }
 
