da78dc0a1e13061f9b64e79734439139417667bb
keketrtr <keketrtr@126.com>
2018-07-01 07:12:54 +0800

    读取配置文件

---
+++ src/main/java/com/example/demo/bean/PropertiesDemo.java
@@ -0,0 +1,55 @@
+package com.example.demo.bean;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@ConfigurationProperties(prefix = "db")
+@Component
+public class PropertiesDemo {
+    private String url;
+    private String username;
+    private String password;
+    private String driverClassName;
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getDriverClassName() {
+        return driverClassName;
+    }
+
+    public void setDriverClassName(String driverClassName) {
+        this.driverClassName = driverClassName;
+    }
+
+    @Override
+    public String toString() {
+        return "PropertiesDemo{" +
+                "url='" + url + '\'' +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", driverClassName='" + driverClassName + '\'' +
+                '}';
+    }
+}
--- src/main/java/com/example/demo/web/Example.java
+++ src/main/java/com/example/demo/web/Example.java
@@ -1,5 +1,6 @@
 package com.example.demo.web;
 
+import com.example.demo.bean.PropertiesDemo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,11 +13,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class Example {
     @Autowired
     private ApplicationArguments applicationArguments;
+    @Autowired
+    private PropertiesDemo propertiesDemo;
 
     @RequestMapping("/")
     public String home(){
         System.out.println("=====================6666666666666666");
         System.out.println(applicationArguments.getNonOptionArgs());
+        System.out.println(propertiesDemo);
         return "Hello World!";
     }
 
